e4e77f091a6321245215695dea93266c95812eee
ricardod31 <ricardo.dantas31@gmail.com>
2018-01-27 15:38:59 -0200

    paginas de erro 403, 404 e 500

--- src/main/java/com/lacosdaalegria/intralacos/controller/navegation/GeneralController.java
+++ src/main/java/com/lacosdaalegria/intralacos/controller/navegation/GeneralController.java
@@ -16,17 +16,7 @@ public class GeneralController {
 	public String logginPage() {
 		return "login";
 	}
-	//Implementar pagina de erro
-	@GetMapping("/erro")
-	public String erroPage() {
-		return "login";
-	}
-	//Implementar pagina de acesso negado
-	@GetMapping("/403")
-	public String acessDeniedPage() {
-		return "login";
-	}
-	
+
 	/**
 	* Lógica de triagem para pagina inicial que deve ser apresentada no momento do acesso
 	*/
